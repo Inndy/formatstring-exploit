a64af0246227629b206254188f7682656a5de962
Inndy <inndy.tw@gmail.com>
2017-03-22 17:01:35 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='formatstring-exploit',
+    version='0.1',
+    description='formatstring-exploit',
+    keywords='formatstring-exploit',
+    url='http://github.com/Inndy/formatstring-exploit',
+    author='Inndy',
+    author_email='inndy.tw@gmail.com',
+    license='MIT',
+    py_modules=['fmtstr'],
+    zip_safe=True
+)
